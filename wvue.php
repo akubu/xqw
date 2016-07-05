5b618c97376da334c2c0d69595add65816ad52c5
akubuzz <akubuzz@gmail.com>
2016-07-05 16:45:03 +0530

    Initial commit

---
+++ wvue.php
@@ -0,0 +1,16 @@
+<html>
+<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+<meta content="utf-8" http-equiv="encoding">
+<head>
+    <script src="vue.js" > </script>
+
+</head>
+<body>
+<div id="example-1">
+   <span v-for="n in 10">{{ n }}</span>
+
+</div>
+
+
+</body>
+</html>
